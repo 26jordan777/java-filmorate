327d86e9a0048c04bba00429a35b9bdae8e601f9
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-02-16 14:08:26 +0300

    Редактирование стиля

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -21,24 +21,29 @@ public class Film {
 
     public void validate() throws ValidationException {
         log.debug("Валидация фильма: {}", this);
-        if (name == null || name.isEmpty()){
+
+        if (name == null || name.isEmpty()) {
             if (log.isErrorEnabled()) {
                 log.error("Ошибка валидации: {}", "Название фильма не может быть пустым.");
             }
             throw new ValidationException("Название фильма не может быть пустым.");
         }
+
         if (description != null && description.length() > 200) {
             log.error("Ошибка валидации: {}", "Максимальная длина описания — 200 символов.");
             throw new ValidationException("Максимальная длина описания — 200 символов.");
         }
+
         if (releaseDate != null && releaseDate.isBefore(LocalDate.of(1895, 12, 28))) {
             log.error("Ошибка валидации: {}", "Дата релиза не может быть раньше 28 декабря 1895 года.");
             throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года.");
         }
+
         if (duration <= 0) {
             log.error("Ошибка валидации: {}", "Продолжительность фильма должна быть положительным числом.");
             throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
         }
+
         log.info("Фильм {} успешно прошел валидацию.", this);
     }
 }
