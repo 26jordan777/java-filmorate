a99532d71563b93862bcb84b936ac025baf98f7f
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-03-12 23:51:37 +0300

    Исправление ошибок

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.time.LocalDate;
 import java.util.*;
 
 @Service
