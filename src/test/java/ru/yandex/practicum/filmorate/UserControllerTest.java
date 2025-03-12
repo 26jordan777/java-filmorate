0ab61bc722819a93b550df6c0ed0a9e8ed0fcae7
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-03-13 00:15:15 +0300

    Исправление ошибок

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -14,7 +14,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.time.LocalDate;
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
