f88f8c552dbd12d12b146218c746ed389ff30076
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-03-19 22:40:25 +0300

    Исправление ошибок тестов

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
