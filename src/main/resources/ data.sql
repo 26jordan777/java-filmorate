01359338700bef61816c440dd5c750bed4aaf402
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-04-24 22:01:34 +0300

    Редактирование

--- src/main/resources/ data.sql
+++ src/main/resources/ data.sql
@@ -1,6 +1,16 @@
- INSERT INTO films (name, description, release_date, duration) VALUES
+INSERT INTO USERS (email, login, name, birthday) VALUES
+   ('user@example.com', 'validLogin', 'Valid Name', '2000-01-01');
+
+INSERT INTO FILMS (name, description, release_date, duration) VALUES
    ('Film 1', 'Description 1', '2023-01-01', 120),
    ('Film 2', 'Description 2', '2023-02-01', 90);
 
-   INSERT INTO users (email, login, name, birthday) VALUES
-   ('user@example.com', 'validLogin', 'Valid Name', '2000-01-01');
+INSERT INTO GENRES (name) VALUES
+   ('Комедия'),
+   ('Драма');
+
+INSERT INTO MPA (name) VALUES
+   ('G'),
+   ('PG'),
+   ('PG-13'),
+   ('R');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,25 @@
-   CREATE TABLE IF NOT EXISTS films (
-       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(255) NOT NULL,
-       description VARCHAR(200),
-       release_date DATE NOT NULL,
-       duration BIGINT NOT NULL
-   );
+  create table if not exists USERS (
+      user_id integer not null primary key auto_increment,
+      email varchar(255) not null,
+      login varchar(255) not null,
+      name varchar(255) not null,
+      birthday date not null
+  );
 
-   CREATE TABLE IF NOT EXISTS users (
-       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-       email VARCHAR(255) NOT NULL,
-       login VARCHAR(255) NOT NULL,
-       name VARCHAR(255),
-       birthday DATE NOT NULL
-   );
+  create table if not exists FILMS (
+      film_id integer primary key auto_increment,
+      name VARCHAR(255) not null,
+      description VARCHAR(200),
+      release_date DATE not null,
+      duration BIGINT not null
+  );
+
+  create table if not exists GENRES (
+      genres_id integer primary key auto_increment,
+      name VARCHAR(255) not null
+  );
+
+  create table if not exists MPA (
+      mpa_id integer primary key auto_increment,
+      name VARCHAR(255) not null
+  );
