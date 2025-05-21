5de6e6c704245703c89fcc1a12122537964a7d3c
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-05-21 22:47:40 +0300

    Исправлнение ошибок

--- src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
@@ -10,7 +10,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.time.LocalDate;
-import java.util.List;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,32 +24,7 @@ public class UserDbStorageTest {
 
     @BeforeEach
     public void setUp() {
-        List<User> users = userDbStorage.getAllUsers();
-        for (User user : users) {
-            userDbStorage.deleteUser(user.getId());
-        }
-        User user = new User();
-        user.setEmail("user@example.com");
-        user.setLogin("validLogin");
-        user.setName("Valid Name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        userDbStorage.addUser(user);
-    }
-
-    @Test
-    public void testAddUser() {
-        User user = new User();
-        user.setEmail("newuser@example.com");
-        user.setLogin("newLogin");
-        user.setName("New User");
-        user.setBirthday(LocalDate.of(1990, 1, 1));
-
-        userDbStorage.addUser(user);
-
-        List<User> users = userDbStorage.getAllUsers();
-        assertThat(users).hasSize(2);
-        assertThat(users.get(1).getEmail()).isEqualTo("newuser@example.com");
+        userDbStorage.addUser(new User("user@example.com", "validLogin", "Valid Name", LocalDate.of(2000, 1, 1)));
     }
 
     @Test
@@ -70,16 +45,12 @@ public class UserDbStorageTest {
         assertThat(updatedUser.getName()).isEqualTo("Updated Name");
     }
 
-
     @Test
     public void testDeleteUser() {
-        User user = new User();
-        user.setEmail("user@example.com");
-        user.setLogin("validLogin");
-        user.setName("Valid Name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
+        User user = userDbStorage.getUserById(1);
         userDbStorage.deleteUser(user.getId());
 
+        User deletedUser = userDbStorage.getUserById(user.getId());
+        assertThat(deletedUser).isNull();
     }
 }
