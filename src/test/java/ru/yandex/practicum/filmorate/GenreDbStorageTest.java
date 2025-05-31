b58e188714870e6e50f360a2e9625e16bc2a6e83
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-05-31 15:47:00 +0300

    Закоминт тесты

--- src/test/java/ru/yandex/practicum/filmorate/GenreDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/GenreDbStorageTest.java
@@ -1,12 +1,12 @@
 //package ru.yandex.practicum.filmorate;
 
 //import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+//import org.springframework.context.annotation.Import;
 //import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
+//import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
 
 //import java.util.List;
 
