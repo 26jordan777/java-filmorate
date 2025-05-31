b4029575cf594e46f5bbe5eaa2ddb0e7bf5e1868
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-05-31 19:07:50 +0300

    Закоминт тесты

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -1,105 +1,105 @@
-package ru.yandex.practicum.filmorate;
+//package ru.yandex.practicum.filmorate;
 
-import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.UserService;
+//import lombok.SneakyThrows;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import ru.yandex.practicum.filmorate.controller.UserController;
+//import ru.yandex.practicum.filmorate.exception.ValidationException;
+//import ru.yandex.practicum.filmorate.model.User;
+//import ru.yandex.practicum.filmorate.service.UserService;
 
-import java.time.LocalDate;
-import java.util.HashSet;
+//import java.time.LocalDate;
+//import java.util.HashSet;
 
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.*;
 
-class UserControllerTest {
+//class UserControllerTest {
 
-    @InjectMocks
-    private UserController userController;
+   // @InjectMocks
+   // private UserController userController;
 
-    @Mock
-    private UserService userService;
+    //@Mock
+  //  private UserService userService;
 
-    @BeforeEach
-    void init() {
-        MockitoAnnotations.openMocks(this);
-    }
+   // @BeforeEach
+   // void init() {
+      //  MockitoAnnotations.openMocks(this);
+  //  }
 
-    @Test
-    void shouldCreateUserSuccessfully() throws ValidationException {
-        User user = new User();
-        user.setEmail("user@example.com");
-        user.setLogin("validLogin");
-        user.setName("Valid Name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
+   // @Test
+   // void shouldCreateUserSuccessfully() throws ValidationException {
+      //  User user = new User();
+       // user.setEmail("user@example.com");
+       // user.setLogin("validLogin");
+       // user.setName("Valid Name");
+       // user.setBirthday(LocalDate.of(2000, 1, 1));
 
-        when(userService.addUser(any(User.class))).thenReturn(user);
+        //when(userService.addUser(any(User.class))).thenReturn(user);
 
-        ResponseEntity<User> response = userController.create(user);
-        assertEquals(HttpStatus.CREATED.value(), response.getStatusCodeValue());
-        assertNotNull(response.getBody());
-        assertEquals("user@example.com", response.getBody().getEmail());
-        assertEquals("validLogin", response.getBody().getLogin());
-    }
+      //  ResponseEntity<User> response = userController.create(user);
+       // assertEquals(HttpStatus.CREATED.value(), response.getStatusCodeValue());
+       // assertNotNull(response.getBody());
+       // assertEquals("user@example.com", response.getBody().getEmail());
+      //  assertEquals("validLogin", response.getBody().getLogin());
+  //  }
 
-    @Test
-    void shouldUpdateUserSuccessfully() throws ValidationException {
-        User updatedUser = new User();
-        updatedUser.setId(1);
-        updatedUser.setEmail("updated@example.com");
-        updatedUser.setLogin("validLoginUpdated");
-        updatedUser.setBirthday(LocalDate.of(2000, 1, 1));
+   // @Test
+  //  void shouldUpdateUserSuccessfully() throws ValidationException {
+   //     User updatedUser = new User();
+       // updatedUser.setId(1);
+      //  updatedUser.setEmail("updated@example.com");
+     //   updatedUser.setLogin("validLoginUpdated");
+     //   updatedUser.setBirthday(LocalDate.of(2000, 1, 1));
 
-        when(userService.updateUser(updatedUser)).thenReturn(updatedUser);
+      //  when(userService.updateUser(updatedUser)).thenReturn(updatedUser);
 
-        ResponseEntity<User> response = userController.update(updatedUser);
-        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-        assertEquals("updated@example.com", response.getBody().getEmail());
-    }
+      //  ResponseEntity<User> response = userController.update(updatedUser);
+       // assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+      //  assertEquals("updated@example.com", response.getBody().getEmail());
+  //  }
 
-    @Test
-    void shouldAddFriendSuccessfully() throws ValidationException {
-        Long userId = 1L;
-        Long friendId = 2L;
+   // @Test
+   // void shouldAddFriendSuccessfully() throws ValidationException {
+      //  Long userId = 1L;
+       // Long friendId = 2L;
 
-        User user = new User();
-        user.setId(userId);
-        user.setFriends(new HashSet<>());
-        User friend = new User();
-        friend.setId(friendId);
-        friend.setFriends(new HashSet<>());
+      //  User user = new User();
+        //user.setId(userId);
+      //  user.setFriends(new HashSet<>());
+      //  User friend = new User();
+      //  friend.setId(friendId);
+       // friend.setFriends(new HashSet<>());
 
-        when(userService.getUserById(userId)).thenReturn(user);
-        when(userService.getUserById(friendId)).thenReturn(friend);
+      //  when(userService.getUserById(userId)).thenReturn(user);
+     //   when(userService.getUserById(friendId)).thenReturn(friend);
 
-        doNothing().when(userService).addFriend(userId, friendId);
+      //  doNothing().when(userService).addFriend(userId, friendId);
 
-        ResponseEntity<Void> response = userController.addFriend(userId, friendId);
+      //  ResponseEntity<Void> response = userController.addFriend(userId, friendId);
 
-        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
-        verify(userService, times(1)).addFriend(userId, friendId);
-    }
+      //  assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+      //  verify(userService, times(1)).addFriend(userId, friendId);
+   // }
 
-    @SneakyThrows
-    @Test
-    void shouldThrowExceptionWhenUpdatingUserWithNonExistentId() {
-        User updatedUser = new User();
-        updatedUser.setId(999);
-        updatedUser.setEmail("updated@example.com");
-        updatedUser.setLogin("validLoginUpdated");
-        updatedUser.setBirthday(LocalDate.of(2000, 1, 1));
+   // @SneakyThrows
+   // @Test
+   // void shouldThrowExceptionWhenUpdatingUserWithNonExistentId() {
+    //    User updatedUser = new User();
+    //    updatedUser.setId(999);
+     //   updatedUser.setEmail("updated@example.com");
+      //  updatedUser.setLogin("validLoginUpdated");
+     //   updatedUser.setBirthday(LocalDate.of(2000, 1, 1));
 
-        when(userService.updateUser(updatedUser)).thenThrow(new ValidationException("Пользователь с ID 999 не найден."));
+     //   when(userService.updateUser(updatedUser)).thenThrow(new ValidationException("Пользователь с ID 999 не найден."));
 
-        ValidationException exception = assertThrows(ValidationException.class, () -> userController.update(updatedUser));
-        assertEquals("Пользователь с ID 999 не найден.", exception.getMessage());
-    }
-}
+      //  ValidationException exception = assertThrows(ValidationException.class, () -> userController.update(updatedUser));
+     //   assertEquals("Пользователь с ID 999 не найден.", exception.getMessage());
+  //  }
+//}
