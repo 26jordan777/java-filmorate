89f6fe04c4a506df84b18e2d3a9edd50d819384a
26jordan777 <ilya.sarzhevskiy96@gmail.com>
2025-06-21 23:44:26 +0300

    Разком ГыUserServiceTest

--- src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
@@ -1,4 +1,4 @@
-/* package ru.yandex.practicum.filmorate;
+ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -56,4 +56,3 @@ class UserServiceTest {
     }
 }
 
- */
